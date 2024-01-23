2118b77bbd2b971517f75b68df849e5872cc6e92
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-01-23 14:35:09 +0530

    -completed exercise 9.10

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diagnosesRouter from './src/routes/diagnoses';
+
 import cors = require('cors');
 const app = express();
 app.use(express.json());
@@ -8,7 +10,7 @@ const PORT = 3001;
 app.get('/api/ping',(_req,res)=>{
     res.send('Hello patient');
 });
-
+app.use('/api/diagnoses',diagnosesRouter);
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`);
 });
